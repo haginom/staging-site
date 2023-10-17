8d7858fc4559d517c353320d5072d7d650b5d288
Saki Reid <saki.reid@gmail.com>
2023-10-17 10:27:00 +0200

    staging data coming from staging? git push

--- cyan-deer/sanity.cli.js
+++ cyan-deer/sanity.cli.js
@@ -3,6 +3,18 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'k1v0o1cy',
-    dataset: 'production'
-  }
+  },
+  graphql: [
+    {
+      tag: 'default',
+      playground: true,
+      id: 'production-workspace',
+      workspace: 'production-workspace',
+    },
+    {
+      playground: true,
+      id: 'staging-workspace',
+      workspace: 'staging-workspace',
+    },
+  ],
 })
