a64b184e164e916f3f9862d34b6369cd75903a69
Saki Reid <saki.reid@gmail.com>
2023-10-17 10:06:39 +0200

    staging

--- cyan-deer/sanity.config.js
+++ cyan-deer/sanity.config.js
@@ -3,16 +3,27 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'cyan-deer',
-
-  projectId: 'k1v0o1cy',
-  dataset: 'production',
-
-  plugins: [deskTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    name: 'production-workspace',
+    title: 'production-workspace',
+    projectId: 'k1v0o1cy',
+    plugins: [deskTool(), visionTool()],
+    basePath: '/production',
+    dataset: 'production',
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'staging-workspace',
+    title: 'staging-workspace',
+    projectId: 'k1v0o1cy',
+    plugins: [deskTool(), visionTool()],
+    basePath: '/staging',
+    dataset: 'staging',
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
